ea0f9c01f154bbe710c89865ff12ff923ffa4f10
slavah8 <vhq5091@psu.edu>
2020-09-01 21:51:52 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,20 @@
+//Author: Vyacheslav Hlushko vqh5091@psu.edu
+//Collaborator Brian Chettle bjc5969@psu.edu
+//Collaborator Ching-Ju Chen cxc6001@psu.edu
+//Collaborator Youngjae Bae yvb5135@psu.edu
+#include <readline/readline.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void) {
+
+char *temp_str = readline("Enter temperature in Celsius:");
+double celsius = atof(temp_str);
+double fahrenheit = (celsius * 9/5) + 32;
+printf("%.6f° in Celsius is equivalent to %.6f° Fahrenheit", celsius, fahrenheit);
+
+
+  
+  return 0;
+}
